9190eaa02bbdef16c27af6be90767f0b44c2951b
Kbell0629 <se2login@gmail.com>
2025-02-26 21:41:33 -0500

    Create get-config.js

---
+++ functions/get-config.js
@@ -0,0 +1,18 @@
+// functions/get-config.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      squareLocationId: process.env.SQUARE_LOCATION_ID,
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID
+      }
+    })
+  };
+};
